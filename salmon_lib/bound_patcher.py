9eb8d90703521e3361bfd86a173d62bfaf68606c
ubuntor <2906473+ubuntor@users.noreply.github.com>
2020-11-20 20:27:43 -0500

    actual arg parsing in bound patcher

--- salmon_lib/bound_patcher.py
+++ salmon_lib/bound_patcher.py
@@ -1,18 +1,35 @@
+import argparse
 import struct
 
-# change these: these are coordinate ranges
-# note that longitude is negated
-# due to silly compiler optimizations, the longitude might be off by +/- 0.00005, but that's probably fine :)
 if __name__ == '__main__':
-    lat = [42.33, 60.2]
-    lon = [104.1, 140.2]
+    parser = argparse.ArgumentParser(
+        description="Patch CRiSP map coordinate bounds.",
+        epilog="Note: due to silly compiler optimizations, the longitude might be off by +/- 0.00005, "
+        "but that's probably fine :)",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("-i", default="crisphv3.exe",
+                        metavar='INFILE', help="input CRiSP executable filename")
+    parser.add_argument("-o", default="crisphv3_new.exe",
+                        metavar='OUTFILE', help="output CRiSP executable filename")
+    parser.add_argument("--lat", type=float, nargs=2, metavar=('MIN', 'MAX'), default=argparse.SUPPRESS,
+                        help="latitude bounds", required=True)
+    parser.add_argument("--lon", type=float, nargs=2, metavar=('MIN', 'MAX'), default=argparse.SUPPRESS,
+                        help="longitude bounds. Note that these are negated (degrees W)", required=True)
 
-    assert lat[0] < lat[1]
-    assert lon[0] < lon[1]
+    args = parser.parse_args()
+    print(args)
+
+    lat = args.lat
+    lon = args.lon
+
+    if lat[0] > lat[1]:
+        lat[0], lat[1] = lat[1], lat[0]
+    if lon[0] > lon[1]:
+        lon[0], lon[1] = lon[1], lon[0]
 
     file_offset = 0x400c00
 
-    with open("crisphv3.exe","rb") as f:
+    with open(args.i, "rb") as f:
         s = bytearray(f.read())
 
     def ud(b):
@@ -27,21 +44,20 @@ if __name__ == '__main__':
     def put4(addr, b):
         s[addr-file_offset:addr-file_offset+4] = b
 
-    old_lon_lo = ud(get4(0x41f9fb) + get4(0x41fa00))
-    old_lon_hi = ud(get4(0x41f9fb) + get4(0x41fa24))
     old_lat_lo = ud(get4(0x41fa13) + get4(0x41fa2b))
     old_lat_hi = ud(get4(0x41fa1a) + get4(0x41fa32))
-    print("old lon (copy 1): [{}, {}]".format(old_lon_lo, old_lon_hi))
-    print("old lat (copy 1): [{}, {}]".format(old_lat_lo, old_lat_hi))
-
     old_lon_lo = ud(get4(0x41f9fb) + get4(0x41fa00))
-    old_lon_hi = ud(get4(0x41f9fb) + get4(0x41fa89))
+    old_lon_hi = ud(get4(0x41f9fb) + get4(0x41fa24))
+    print(f"old bounds (copy 1): lat=[{old_lat_lo}, {old_lat_hi}], lon=[{old_lon_lo}, {old_lon_hi}]")
+
     old_lat_lo = ud(get4(0x41fa78) + get4(0x41fa90))
     old_lat_hi = ud(get4(0x41fa7f) + get4(0x41fa97))
-    print("old lon (copy 2): [{}, {}]".format(old_lon_lo, old_lon_hi))
-    print("old lat (copy 2): [{}, {}]".format(old_lat_lo, old_lat_hi))
+    old_lon_lo = ud(get4(0x41f9fb) + get4(0x41fa00))
+    old_lon_hi = ud(get4(0x41f9fb) + get4(0x41fa89))
+    print(f"old bounds (copy 2): lat=[{old_lat_lo}, {old_lat_hi}], lon=[{old_lon_lo}, {old_lon_hi}]")
 
     # don't touch 0x41f9fb: it's shared between lon_lo and lon_hi
+    # fortunately, it's the least significant part of lon_lo and lon_hi
 
     lon_lo = pd(lon[0])
     put4(0x41fa00, lon_lo[4:])
@@ -62,19 +78,17 @@ if __name__ == '__main__':
     put4(0x41fa7f, lat_hi[:4])
     put4(0x41fa97, lat_hi[4:])
 
-    new_lon_lo = ud(get4(0x41f9fb) + get4(0x41fa00))
-    new_lon_hi = ud(get4(0x41f9fb) + get4(0x41fa24))
     new_lat_lo = ud(get4(0x41fa13) + get4(0x41fa2b))
     new_lat_hi = ud(get4(0x41fa1a) + get4(0x41fa32))
-    print("new lon (copy 1): [{}, {}]".format(new_lon_lo, new_lon_hi))
-    print("new lat (copy 1): [{}, {}]".format(new_lat_lo, new_lat_hi))
-
     new_lon_lo = ud(get4(0x41f9fb) + get4(0x41fa00))
-    new_lon_hi = ud(get4(0x41f9fb) + get4(0x41fa89))
+    new_lon_hi = ud(get4(0x41f9fb) + get4(0x41fa24))
+    print(f"new bounds (copy 1): lat=[{new_lat_lo}, {new_lat_hi}], lon=[{new_lon_lo}, {new_lon_hi}]")
+
     new_lat_lo = ud(get4(0x41fa78) + get4(0x41fa90))
     new_lat_hi = ud(get4(0x41fa7f) + get4(0x41fa97))
-    print("new lon (copy 2): [{}, {}]".format(new_lon_lo, new_lon_hi))
-    print("new lat (copy 2): [{}, {}]".format(new_lat_lo, new_lat_hi))
+    new_lon_lo = ud(get4(0x41f9fb) + get4(0x41fa00))
+    new_lon_hi = ud(get4(0x41f9fb) + get4(0x41fa89))
+    print(f"new bounds (copy 2): lat=[{new_lat_lo}, {new_lat_hi}], lon=[{new_lon_lo}, {new_lon_hi}]")
 
-    with open("crisphv3_new.exe","wb") as f:
+    with open(args.o, "wb") as f:
         f.write(s)
