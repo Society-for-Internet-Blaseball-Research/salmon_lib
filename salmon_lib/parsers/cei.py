4d25ab2e91f1ef65d65f46a22ba2b27f5cd57edb
RLB <20372070+robbyblum@users.noreply.github.com>
2020-11-21 22:23:14 -0500

    mistakes will be made

---
+++ salmon_lib/parsers/cei.py
@@ -0,0 +1,16 @@
+"""
+let's do this
+this file is arcane. quoth the manual:
+
+The *.cei files (see Fig. 2.11) is used to set catch ceilings which are the
+primary means selected by the PSC to reduce stock exploitation rates. The *.cei
+file is used: (1) to specify fisheries with ceilings; (2) to set ceiling levels
+(catch levels); and (3) to allow you to force Model catches to equal the
+ceiling.
+
+
+"""
+
+
+def parse_cei():
+    pass
