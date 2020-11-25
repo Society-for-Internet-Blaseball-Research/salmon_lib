c48abb42ae2f12faa3d26cdd2dcb9d7a5ca24930
Alis W <waylandalis@gmail.com>
2020-11-25 11:11:59 -0300

    dependencies stuff

--- setup.py
+++ setup.py
@@ -2,14 +2,13 @@ from setuptools import setup
 
 setup(
     name="salmon_lib",
-    version="0.3.0",
+    version="0.3.1",
     description="a library to read and write CRiSP harvest files",
     url="https://github.com/Society-for-Internet-Blaseball-Research/salmon_lib",
     author="alisww",
     author_email="waylandalis@gmail.com",
     license="MIT",
     packages=["salmon_lib", "salmon_lib.parsers", "salmon_lib.resources"],
-    install_requires=["matplotlib==3.3.3"],
     include_package_data=True,
     zip_safe=False,
 )
